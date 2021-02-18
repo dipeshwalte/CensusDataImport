3762dd3e69be2552c13bde1f9d76775a2e618ba8
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-18 16:06:19 +0530

    Add project files.

---
+++ CensusDataImport/CensusAnalyser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CensusDataImport
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class CensusAnalyser
+    {
+        public Dictionary<string, CensusDataRow> datamap;
+        /// <summary>
+        /// Loads the census data.
+        /// </summary>
+        /// <param name="csvFilePath">The CSV file path.</param>
+        /// <param name="dataHeaders">The data headers.</param>
+        /// <returns></returns>
+        /// <exception cref="Dictionary<string, CensusDataRow>"></exception>
+        public Dictionary<string, CensusDataRow> LoadCensusData(string csvFilePath, string dataHeaders)
+        {
+            datamap = new Dictionary<string, CensusDataRow>();
+            // Throw File not exists
+            // Throw Improper extension
+
+            string[] censusData = File.ReadAllLines(csvFilePath);
+            if (censusData[0] != dataHeaders)
+            {
+                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.INCORRECT_HEADER, "Incorrect Header");
+            }
+            foreach (string row in censusData)
+            {
+                if (!row.Contains(","))
+                {
+                    throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.DELIMITER_NOT_FOUND, "Delimiter not found");
+                }
+                string[] column = row.Split(',');
+                datamap.Add(column[1], new CensusDataRow(column[0], column[1], column[2], column[3]));
+            }   
+            return datamap;
+        }
+    }
+}
---
+++ CensusDataImport/CensusDataRow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusDataImport
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class CensusDataRow
+    {
+        string state;
+        int population;
+        int area;
+        int density;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CensusDataRow"/> class.
+        /// </summary>
+        /// <param name="dataRow">The data row.</param>
+        public CensusDataRow(CensusDataRow dataRow)
+        {
+            this.state = dataRow.state;
+            this.population = dataRow.population;
+            this.area = dataRow.area;
+            this.density = dataRow.density;
+        }
+        public CensusDataRow(string state,string population,string area,string density)
+        {
+            this.state = state;
+            this.population = Convert.ToInt32(population);
+            this.area = Convert.ToInt32(area);
+            this.density = Convert.ToInt32(density);
+        }
+    }
+}
